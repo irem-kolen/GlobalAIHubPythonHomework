41499543d8837e0b0722e861d72c68ed72516ce0
irem-kolen <76458094+irem-kolen@users.noreply.github.com>
2020-12-27 19:16:16 +0300

    Add files via upload

---
+++ HW-2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+while True :
+    a=input("first name:") 
+    b=input("last name:")
+    c=int(input("age:"))
+    d=int(input("date of birth:"))
+
+   
+    if c < 18  :
+               print("You can't go out because it's too dangerous")
+    elif c > 18 :
+            print("You can go out to the street")
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
