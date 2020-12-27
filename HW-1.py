53296656174c253f09890de48290d7d92640cde5
irem-kolen <76458094+irem-kolen@users.noreply.github.com>
2020-12-27 19:59:41 +0300

    Add files via upload

---
+++ HW-1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+deger1=int(input("deger giriniz:"))
+print(type(deger1))
+deger2=int(input("deger giriniz:"))
+print(type(deger2))
+deger3=input("deger giriniz:")
+print(type(deger3))
+deger4=input("deger giriniz:")
+print(type(deger4))
+deger5=input("deger giriniz:")
+print(type(deger5))
+
+
+print("1:{} and 2:{} and 3:{} and 4:{} and 5:{} ".format(deger1,deger2,deger3,deger4,deger5))  
+
+print(f'Sayıların toplamı {deger1+deger2} eder.' )
+
+        
+
+
+# In[ ]:
+
+
+
+
